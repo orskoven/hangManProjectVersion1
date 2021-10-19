db6a311c5fb583d9b6f8251e2092eb7c38bfb6ff
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2021-10-19 13:08:20 +0200

    made some good progress

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
         GameMode gameTest = new GameMode();
-        gameTest.getGameFunctionalities('a',"abe");
+        gameTest.getGameFunctionalities("abe");
     }
 }
--- src/GameMode.java
+++ src/GameMode.java
@@ -1,22 +1,55 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.function.Predicate;
 
-public class GameMode{
+public class GameMode {
+    static Scanner scanner = new Scanner(System.in);
     private ArrayList<Character> characterArrayList = new ArrayList<>();
+    private ArrayList<Character> characterArrayListToBeGuessed = new ArrayList<>();
     private String charToString = "";
     private String[] stringToContainAnswers = charToString.split(",");
 
     public GameMode() {
     }
 
-    public void getGameFunctionalities(char charGuess, String wordToBeGuessed){
-        for (int i = 0; i < wordToBeGuessed.length() ; i++) {
+    public void getGameFunctionalities(String wordToBeGuessed) {
+        for (int i = 0; i < wordToBeGuessed.length(); i++) {
+            characterArrayListToBeGuessed.add(wordToBeGuessed.charAt(i));
             characterArrayList.add('_');
-            charToString = characterArrayList.toString();
-        }
-        System.out.println(characterArrayList);
-        System.out.println(Arrays.toString(stringToContainAnswers));
 
+            //Inspiration from : https://stackoverflow.com/questions/36170017/swapping-out-elements-between-arraylists
+        }
+        for (int i = 0, j = 0; i < characterArrayListToBeGuessed.size() && j < characterArrayList.size(); i++, j++) {
+            System.out.println("Please type a letter to guess: ");
+            char inputCharGuess = scanner.next().charAt(0);
+            characterArrayList.add(inputCharGuess);
+            if (characterArrayListToBeGuessed.contains(inputCharGuess) && characterArrayList.get(i) == characterArrayList.get(j)) {
+
+                characterArrayListToBeGuessed.set(i, characterArrayList.set(j, characterArrayListToBeGuessed.get(i)));
+            } else {
+                i--;
+                j--;
+
+            }
+            //System.out.println(characterArrayListToBeGuessed);
+            System.out.println(characterArrayList);
+        }
+
+
+        }
     }
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
